d311bd960e422d9b4e2e3ac015668b30d4e8fc65
Eugene L <lugovoj20@gmail.com>
2026-05-16 20:43:42 +0200

    Implement ArrayList logic

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,101 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private final static int MIN_CAPACITY = 10;
+    private int currentCapacity;
+    private Object[] data;
+    private int size;
+
+    public ArrayList() {
+        this.data = new Object[MIN_CAPACITY];
+        currentCapacity = MIN_CAPACITY;
+        this.size = 0;
+    }
+
     @Override
     public void add(T value) {
-
+        grow();
+        data[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index == size) {
+            add(value);
+            return;
+        }
+        checkIndex(index);
+        grow();
+        System.arraycopy(data, index, data, index + 1, size + 1 - index);
+        data[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) data[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        data[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T value = get(index);
+        System.arraycopy(data, index + 1, data, index, size - index - 1);
+        data[--size] = null;
+        return value;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(data[i], element)) {
+                index = i;
+                break;
+            }
+        }
+        return index == -1 ? null : remove(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void grow() {
+        if (size + 1 > currentCapacity) {
+            int newCapacity = currentCapacity + (currentCapacity >> 1);
+            Object[] newData = new Object[newCapacity];
+            System.arraycopy(data, 0, newData, 0, data.length);
+            currentCapacity = newCapacity;
+            data = newData;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", size: " + size);
+        }
     }
 }
